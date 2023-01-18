5144b197281e47b647fc7c77b94ce4d950b918bb
Sergey Sevostyanov <sermsev@yandex.ru>
2023-01-18 18:29:06 +1000

    Added подсчет времени к Task_32 with data-files

---
+++ Home_work_6/Task_4.py
@@ -0,0 +1,44 @@
+# 4.Создайте список из случайных чисел. Найдите количество различных элементов в нем.
+
+import random
+from time import perf_counter
+
+# Генерируем случайный набор данных
+n = int(input('Сколько будет элементов? '))
+list_in = []
+for i in range(n):
+    list_in.append(random.randint(1, 10))
+print('Сгенерированный набор данных -', list_in)
+
+# Решаем задачу
+print()
+print('1-ый вариант - список, цикл for')
+start = perf_counter()
+list_out = []
+for el in list_in:
+    if el not in list_out:
+        list_out.append(el)
+end = perf_counter()
+print('Различных элементов в списке - ', len(list_out))
+print('Список неповторяющихся элементов последовательности (в порядке появления): ', list_out)
+print('Вермя работы - ', round(end - start, 6))
+
+print()
+print('2-ой вариант - списковое включение (list comprehension)')
+start = perf_counter()
+list_out = []
+[list_out.append(el) for el in list_in if el not in list_out]
+end = perf_counter()
+print('Различных элементов в списке - ', len(list_out))
+print('Список неповторяющихся элементов последовательности (в порядке появления): ', list_out)
+print('Вермя работы - ', round(end - start, 6))
+
+print()
+print('3-ий вариант - множество')
+start = perf_counter()
+set_out = set()
+[set_out.add(el) for el in list_in]
+end = perf_counter()
+print('Различных элементов в списке - ', len(set_out))
+print('Множество неповторяющихся элементов последовательности : ', set_out)
+print('Вермя работы - ', round(end - start, 6))
